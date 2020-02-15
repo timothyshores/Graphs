2713ff1de92cbd5ffc0d80b2c20565c80d866391
Timothy Shores <timothymshores@gmail.com>
2020-02-15 13:06:32 -0600

    Update ancestors.py using DFS and a Stack

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -26,7 +26,7 @@ def earliest_ancestor(ancestors, starting_node):
     def get_parents(child):
         """
         returns array of parents (neighbors) of child (value)
-        graph will be DIRECTIONAL -- we only want to consider ancestors, so
+        graph will be DIRECTIONAL -- we only want to consider parent ancestors, so
         we will only search upwards through the parental heritage, never searching downward
         """
         return [pair[0] for pair in ancestors if pair[1] == child]
@@ -63,6 +63,6 @@ def earliest_ancestor(ancestors, starting_node):
     return ancestor_path[-1]
 
 
-# test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
-#                   (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+print([pair[0] for pair in ancestors if pair[1] == 1])
 # print(earliest_ancestor(test_ancestors, 1))
